9f2ff0ac061b8fa70f1d5fe556a13d678b9fe6a3
Sreerag <msratofficial@gmail.com>
2022-09-26 22:20:02 +0530

    feat:style head component

--- src/comp/Head.js
+++ src/comp/Head.js
@@ -7,8 +7,7 @@ const Head = () => {
   const {setSearch,result,getData}=useLocation();
     const [ip,setIp]=useState("");
 
-    const getLocation=(e)=>{
-          e.preventDefault();
+    const getLocation=()=>{
           console.log(ip);
           setSearch(ip);
           getData();
@@ -17,17 +16,27 @@ const Head = () => {
 
 
     return (
-        <div className="head-container">
-        <div className="container">
-        <h1 className="text-center">IP Adress Tracker</h1>
-          <form className='d-flex justify-content-center head-form' onClick={(e)=>getLocation(e)}>
-            <div className="input-group row form-container">
-              <input type="text" className="form-control form-input-color col-xl-11 col-10 " 
-              value={ip} onChange={(e)=>setIp(e.target.value)} />
-                <button className="btn btn-primary btn-color col-xl-1 col-2	"><img src={arrow} alt="arrow" /></button>  
+        <div className="head-container ">
+            <h1 className="text-center pt-5 pb-3">IP Adress Tracker</h1>
+          <div className="container d-flex justify-content-center">
+                <div className="input-group mb-3 input-container ">
+                    <input type="text" className="form-control input-radious"  
+                    aria-label="Recipient's username" aria-describedby="button-addon2"
+                    placeholder='Search for any ip address or domain'
+                    value={ip}
+                    onChange={(e)=>setIp(e.target.value)}
+                    />
+                    <button 
+                    className="btn btn-outline-secondary
+                    button
+                    btn btn-primary btn-lg"
+                    type="submit"
+                    onClick={()=>getLocation()}
+                    >
+                      <img src={arrow} className="image-fluid"  />
+                    </button>
+                </div>
             </div>
-          </form>
-          </div>
         </div>
       );
 }
