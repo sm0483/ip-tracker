705ee2e92f7f50bc6bdf54d0c84906908c4806b0
sreerag <msratofficial@gmail.com>
2022-09-29 15:43:32 +0530

    feat:add style for all structure

--- src/comp/Details.js
+++ src/comp/Details.js
@@ -5,8 +5,51 @@ const Details = () => {
     
 
     return(
-      <section>
-        
+      <section className="details-container">
+        <div className="container detail-container">
+          <span className="detail-head">
+            ip address
+          </span>
+          <span className="detail-subhead">
+            {result.query}
+          </span>
+        </div>     
+
+        <div className="underline"></div> 
+
+        <div className="container detail-container">
+          <span className="detail-head">
+            location
+          </span>
+          <span className="detail-subhead">
+            {result.city} {result.region} {result.country}
+          </span>
+        </div>   
+
+        <div className="underline"></div> 
+
+
+        <div className="container detail-container">
+          <span className="detail-head">
+            timezone
+          </span>
+          <span className="detail-subhead">
+            {result.timezone}
+          </span>
+        </div> 
+
+        <div className="underline"></div> 
+
+
+        <div className="container detail-container">
+          <span className="detail-head">
+            isp
+          </span>
+          <span className="detail-subhead">
+            { result.isp && 
+            result.isp.substring(0,17)}
+          </span>
+        </div> 
       </section>
     )
 }
