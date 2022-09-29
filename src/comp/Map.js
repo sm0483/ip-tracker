a201622afd730676b3d5e12914efc21c490ce029
sreerag <msratofficial@gmail.com>
2022-09-29 15:42:48 +0530

    feat: style map components

--- src/comp/Map.js
+++ src/comp/Map.js
@@ -4,7 +4,7 @@ const Map = () => {
     const {result}=useLocation();
     const {country,city,regionName}=result;
     const url=`
-        https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_KEY}&q=${city},${regionName}+${country}
+        https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_KEY}&q=${city},${regionName}+${country} &zoom=15
     `
     return (
         <div className="container-fluid map-container">
